a3771577fd5d4e3ac928e9e06100a5c5d85bbed6
paramsj <jaiparameshwaran01@gmail.com>
2026-09-24 09:56:59 +0530

    refactor1

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,22 @@ package main
 import ("testing")
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, world"
+	t.Run("saying hello to people", func(t *testing.T){
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		assertCorrectMessage(t,got,want)
+	})
 
+	t.Run("say hello without params", func(t *testing.T){
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t,got,want)
+	})
+}
+
+func assertCorrectMessage(t *testing.T, got string, want string) {
+	t.Helper() 
 	if got != want {
-		t.Errorf("got %q want %q",got, want)
+		t.Errorf("got %q want %q", got, want)
 	}
 }
--- main.go
+++ main.go
@@ -2,10 +2,20 @@ package main
 
 import "fmt"
 
-func Hello() string {
-	return "Hello, world"
+func Hello(name string) string {
+	if name == "" {
+		name = "World"
+	}
+	return prefix + name
+}
+
+// no overloading, write what you need
+// capital letter will export, else internal only
+const prefix = "Hello, "
+func HelloWithoutName() string {
+	return prefix
 }
 
 func main() {
-	fmt.Println(Hello())
+	fmt.Println(HelloWithoutName())
 }
