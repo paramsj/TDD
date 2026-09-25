59b2c1aaaf775c183d69a5876d65aa19a9936483
paramsj <jaiparameshwaran01@gmail.com>
2026-09-25 10:19:02 +0530

    structs + interfaces

---
+++ structs/shapes.go
@@ -0,0 +1,28 @@
+package structs
+
+import "math"
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Width + r.Height)
+}
+
+func (r Rectangle) Area() float64 {
+	return (r.Width * r.Height)
+}
+
+func (c Circle) Perimeter() float64 {
+	return 2 * (c.Radius) * math.Pi
+}
+
+func (c Circle) Area() float64 {
+	return (c.Radius * c.Radius) * math.Pi
+}
