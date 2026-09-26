cee752bd43907f05e7032a05da493e33a35d48cd
paramsj <jaiparameshwaran01@gmail.com>
2026-09-26 10:25:38 +0530

    maps + pointers + mocking

--- structs/shapes.go
+++ structs/shapes.go
@@ -2,6 +2,10 @@ package structs
 
 import "math"
 
+type Shape interface {
+	Area() float64
+	Perimeter() float64
+}
 type Rectangle struct {
 	Width  float64
 	Height float64
@@ -9,6 +13,11 @@ type Rectangle struct {
 
 type Circle struct {
 	Radius float64
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
 }
 
 func (r Rectangle) Perimeter() float64 {
@@ -25,4 +34,11 @@ func (c Circle) Perimeter() float64 {
 
 func (c Circle) Area() float64 {
 	return (c.Radius * c.Radius) * math.Pi
+}
+
+func (t Triangle) Area() float64 {
+	return 0.5 * t.Base * t.Height
+}
+func (t Triangle) Perimeter() float64 {
+	return 0.5 * t.Base * t.Height
 }
